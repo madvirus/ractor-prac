60b9dd6390446396f1124060342b74cbc960c7f5
madvirus <madvirus@madvirus.net>
2018-07-13 18:05:02 +0900

    map

--- reactor-basic/src/test/java/transform/TransformTest.java
+++ reactor-basic/src/test/java/transform/TransformTest.java
@@ -2,6 +2,7 @@ package transform;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class TransformTest {
     @Test
@@ -17,4 +18,23 @@ public class TransformTest {
                 .flatMap(i -> Flux.range(1, i))
                 .subscribe(System.out::println);
     }
+
+    @Test
+    void filter() {
+        Flux.range(1, 10)
+                .filter(num -> num % 2 == 0)
+                .subscribe(x -> System.out.print(x + " -> "));
+    }
+
+    @Test
+    void defaultIfEmpty() {
+        Mono.empty()
+                .defaultIfEmpty("empty1")
+                .subscribe(x -> System.out.println(x));
+
+        Flux.empty()
+                .defaultIfEmpty("empty2")
+                .subscribe(System.out::println);
+    }
+
 }
