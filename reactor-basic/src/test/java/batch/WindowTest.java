f622ff26074cb82c0f787e8136af10380ddb5590
madvirus <madvirus@madvirus.net>
2018-08-03 18:00:54 +0900

    parallel

---
+++ reactor-basic/src/test/java/batch/WindowTest.java
@@ -0,0 +1,15 @@
+package batch;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+public class WindowTest {
+    @Test
+    void windowByCount() {
+//        Flux.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+//                .window(3)
+//                .subscribe((Flux<Integer> group) -> {
+//                    group
+//                });
+    }
+}
