4588674360bb45f588716b9903d2a2d58315fc19
madvirus <madvirus@madvirus.net>
2018-08-06 16:40:27 +0900

    retry, error

--- reactor-basic/src/test/java/batch/WindowTest.java
+++ reactor-basic/src/test/java/batch/WindowTest.java
@@ -6,10 +6,18 @@ import reactor.core.publisher.Flux;
 public class WindowTest {
     @Test
     void windowByCount() {
-//        Flux.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-//                .window(3)
-//                .subscribe((Flux<Integer> group) -> {
-//                    group
-//                });
+        Flux.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .log()
+                .window(4, 1)
+                .subscribe((Flux<Integer> group) -> {
+
+                });
+    }
+
+    @Test
+    void buffer() {
+        Flux.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .buffer(4)
+                .subscribe(System.out::println);
     }
 }
